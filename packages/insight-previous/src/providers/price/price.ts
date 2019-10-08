c16199f96be68eb2bfdf1335fd231c5cc0e29adc
Sfxdx <monstrobishi@gmail.com>
2019-10-08 17:25:39 +0700

    insight-previous dfc support added

--- packages/insight-previous/src/providers/price/price.ts
+++ packages/insight-previous/src/providers/price/price.ts
@@ -26,8 +26,8 @@ export class PriceProvider {
     if (currency === 'USD') {
       const ratesAPI =
         this.api.getConfig().chain === 'BTC'
-          ? this.api.ratesAPI.btc
-          : this.api.ratesAPI.bch;
+          ? this.api.ratesAPI.btc : this.api.getConfig().chain === 'BCH'
+          ? this.api.ratesAPI.bch : this.api.ratesAPI.dfc;
       this.api.httpClient.get(ratesAPI).subscribe(
         (data: any) => {
           const currencyParsed: any = data;
