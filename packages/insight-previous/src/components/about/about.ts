e69ba74b04242d3e9cba649155571ec3b09a2b75
Harsh <fullstackninja864@gmail.com>
2020-06-26 19:58:05 +0530

    Update quick stats and about, add resources

--- packages/insight-previous/src/components/about/about.ts
+++ packages/insight-previous/src/components/about/about.ts
@@ -2,6 +2,41 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { DefaultProvider } from '../../providers/default/default';
+import { RedirProvider } from '../../providers/redir/redir';
+
+const defistatsResponse = {
+  id: 'DFI',
+  rewards: {
+    foundationPercent: 10,
+    total: 200,
+    foundation: 20,
+    minter: 180
+  },
+  tokens: {
+    max: 1200000000,
+    supply: {
+      total: 609360200,
+      circulation: 319104180,
+      foundation: 288120000,
+      community: 2136020
+    },
+    initDist: {
+      total: 588000000,
+      totalPercent: 49,
+      foundation: 288120000,
+      foundationPercent: 49,
+      circulation: 299880000,
+      circulationPercent: 51
+    }
+  },
+  chain: 'main',
+  blockHeight: 106801,
+  bestBlockHash:
+    'cc2751f1f9a27a5a72b8f0b59a6dc63aebb92446627b48dc6d54c063da4bb0e5',
+  difficulty: '466809430.2729005',
+  medianTime: 1593165772,
+  timeStamp: 1593165907601
+};
 
 @Component({
   selector: 'about',
@@ -13,7 +48,8 @@ export class AboutComponent implements OnInit {
   public loading = true;
   constructor(
     private httpClient: HttpClient,
-    private defaultProvider: DefaultProvider
+    private defaultProvider: DefaultProvider,
+    private redirProvider: RedirProvider
   ) {}
   public ngOnInit(): void {
     this.loadQuickUpdate().subscribe(
@@ -46,9 +82,18 @@ export class AboutComponent implements OnInit {
       })
     };
 
-    return this.httpClient.get<any>(
-      this.defaultProvider.getDefault('%DEFI_CHAIN_STATS%'),
-      httpOptions
-    );
+    // return this.httpClient.get<any>(
+    //   this.defaultProvider.getDefault('%DEFI_CHAIN_STATS%'),
+    //   httpOptions
+    // );
+    return new Observable(observer => {
+      observer.next(defistatsResponse);
+    });
+  }
+
+  public goToRichList() {
+    const chain = this.defaultProvider.getDefault('%CHAIN%');
+    const network = this.defaultProvider.getDefault('%NETWORK%');
+    this.redirProvider.redir('rich-list', { chain, network });
   }
 }
