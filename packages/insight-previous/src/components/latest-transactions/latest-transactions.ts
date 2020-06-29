e85ba37deabce56948ad9a6ee7a4ff25c961d6aa
Harsh <fullstackninja864@gmail.com>
2020-06-29 12:31:22 +0530

    WIP websocket setup and add providers for websocket

--- packages/insight-previous/src/components/latest-transactions/latest-transactions.ts
+++ packages/insight-previous/src/components/latest-transactions/latest-transactions.ts
@@ -1,51 +1,35 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, Input, NgZone, OnChanges } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ApiProvider } from '../../providers/api/api';
 import { CurrencyProvider } from '../../providers/currency/currency';
 import { Logger } from '../../providers/logger/logger';
 import { RedirProvider } from '../../providers/redir/redir';
+import { WebsocketProvider } from '../../providers/websocket/websocketProvider';
 
 @Component({
   selector: 'latest-transactions',
   templateUrl: 'latest-transactions.html'
 })
-export class LatestTransactionsComponent implements OnChanges {
+export class LatestTransactionsComponent implements OnInit {
   @Input()
   public refreshSeconds = 10;
-  private timer: any;
   public loading = true;
   public transactions = [];
   public errorMessage;
 
   constructor(
-    private httpClient: HttpClient,
     private apiProvider: ApiProvider,
     public currency: CurrencyProvider,
-    private ngZone: NgZone,
     public redirProvider: RedirProvider,
-    private logger: Logger
-  ) {
+    private logger: Logger,
+    private websocketProvider: WebsocketProvider
+  ) { }
+
+  public ngOnInit(): void {
     this.loadTransactions();
   }
 
-  public ngOnChanges(): void {
-    if (this.timer) {
-      clearInterval(this.timer);
-    }
-
-    this.ngZone.runOutsideAngular(() => {
-      this.timer = setInterval(() => {
-        this.ngZone.run(() => {
-          this.loadTransactions.call(this);
-        });
-      }, 1000 * this.refreshSeconds);
-    });
-  }
-
   private loadTransactions(): void {
-    const url: string = this.apiProvider.getUrl() + 'txs';
-
-    this.httpClient.get(url).subscribe(
+    this.websocketProvider.messages.subscribe(
       (data: any) => {
         this.transactions = JSON.parse(data._body);
         this.loading = false;
