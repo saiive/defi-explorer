979780662be239bfa5177854ca170f378bdd4537
Saurabh Shinde <saurabhshinde391@gmail.com>
2020-09-22 18:40:59 +0530

    UI Fixes: Transaction table

--- packages/insight-previous/src/components/coin/coin.ts
+++ packages/insight-previous/src/components/coin/coin.ts
@@ -1,23 +1,40 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ApiProvider } from '../../providers/api/api';
 import { CurrencyProvider } from '../../providers/currency/currency';
 import { RedirProvider } from '../../providers/redir/redir';
-import { AppCoin } from '../../providers/transactions/transactions';
+import { AppCoin, TxsProvider } from '../../providers/transactions/transactions';
+
 @Component({
   selector: 'coin',
   templateUrl: 'coin.html',
 })
-export class CoinComponent {
+export class CoinComponent implements OnInit {
   @Input()
-  public coin: AppCoin | {} = {};
+  public coin: AppCoin | any = {};
   @Input()
   public collapse: boolean = false;
+  @Input()
+  public txInfo: any = {};
+  @Input()
+  public blockTime: any = {};
 
   constructor(
     public apiProvider: ApiProvider,
     public currencyProvider: CurrencyProvider,
-    public redirProvider: RedirProvider
-  ) {}
+    public redirProvider: RedirProvider,
+    public txsProvider: TxsProvider
+  ) { }
+
+  public ngOnInit(): void {
+    const { mintTxid, spentTxid } = this.coin;
+    this.txsProvider.getTx(mintTxid || spentTxid)
+      .subscribe(
+        response => {
+          this.txInfo = response;
+          this.blockTime = new Date(response.blockTime).getTime() / 1000;
+        }
+      );;
+  }
 
   public goToTx(txId: string): void {
     this.redirProvider.redir('transaction', {
