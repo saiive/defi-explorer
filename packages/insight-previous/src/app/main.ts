21b4b096ed0eaeb7590de8c2be0f18bbb132b7f6
DeFiDev <defidev@cakedefi.com>
2020-11-04 15:03:11 +0700

    Fix build prod

--- packages/insight-previous/src/app/main.ts
+++ packages/insight-previous/src/app/main.ts
@@ -3,7 +3,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
 
 import { enableProdMode } from '@angular/core';
-
+//@ts-ignore
 if (process.env.ENV === 'prod') {
   enableProdMode();
 }
