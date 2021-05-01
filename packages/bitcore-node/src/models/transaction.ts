60c8d00e169c273df61469f3b99fa62466d28d85
Patrik Pfaffenbauer <patrik.pfaffenbauer@p3-software.eu>
2021-05-01 21:51:36 +0200

    check if chain is DFI for custom ops

--- packages/bitcore-node/src/models/transaction.ts
+++ packages/bitcore-node/src/models/transaction.ts
@@ -367,11 +367,11 @@ export class TransactionModel extends BaseModel<ITransaction> {
     chain: string;
     network: string;
   }) {
-    if(params.chain != "DFI") 
+    if(params.chain.toUpperCase() != "DFI") 
     {
       return void 0;
     }
-    
+
     let customOps = new Array<CustomOp>();
     for (let tx of params.txs) {
       if (tx.checkCustom().isCustom) {
